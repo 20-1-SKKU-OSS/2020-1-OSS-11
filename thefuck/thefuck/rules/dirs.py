867109df6c8f601fe2b7a737b9d08ded2fa68a64
youngtae47 <63634948+youngtae47@users.noreply.github.com>
2020-06-07 20:57:36 +0900

    modify dirs argument case & input dirs case
    전에 고려하지 못 했던 dirs오탈자 뒤에 argument를 제대로 받게 수정하였고, 정확한 인자 dirs가 들어왔을 때에는 연산할 필요가 없으므로 이 케이스를 하나 더 추가했습니다.

--- thefuck/thefuck/rules/dirs.py
+++ thefuck/thefuck/rules/dirs.py
@@ -1,10 +1,14 @@
 from difflib import SequenceMatcher
 
 def match(command):
+    if SequenceMatcher(None, command.script_parts[0], "dirs").ratio() == 1:
+        return 0
     if SequenceMatcher(None, command.script_parts[0], "dirs").ratio() > 0.6:
         return 1
     else:
         return 0
 
 def get_new_command(command):
-    return "dirs"
+    new_cmds = ["dirs"]
+    return [' '.join([new_command] + command.script_parts[1:])
+            for new_command in new_cmds]
