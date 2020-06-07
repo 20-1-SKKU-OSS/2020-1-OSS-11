367433e09bbb1789a19d4ffe0b296d925ea8c449
Jokuna <rafik57@g.skku.edu>
2020-06-07 20:05:08 +0900

    [Edit] Misprint

--- thefuck/thefuck/rules/nvm.py
+++ thefuck/thefuck/rules/nvm.py
@@ -1,7 +1,7 @@
 from difflib import SequenceMatcher
 
 def match(command):
-   if SequenceMatcher(None, command.script_parts[0], "nvm").ratio() > 0.6:
+    if SequenceMatcher(None, command.script_parts[0], "nvm").ratio() > 0.6:
         return 1
     else:
         return 0
